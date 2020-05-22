45cfc2d8312abbed1991aec2ef2e8e38522911c3
xiatong <vanessazxt@gmail.com>
2020-05-22 15:39:36 +0200

    use env var to create users

--- scripts/user_creator.py
+++ scripts/user_creator.py
@@ -1,9 +1,11 @@
 from airflow import models, settings
 from airflow.contrib.auth.backends.password_auth import PasswordUser
+import os
+
 user = PasswordUser(models.User())
-user.username = 'airflow'
-# user.email = 'email'
-user.password = 'airflow'
+user.username = os.environ['AA_USERNAME']
+user.email = os.environ['AA_EMAIL']
+user.password = ['AA_PASSWORD']
 session = settings.Session()
 session.add(user)
 session.commit()
