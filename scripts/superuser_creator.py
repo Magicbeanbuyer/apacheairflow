bc6ba9298cc2d4f0f8887d775cf9416a5ba69ad9
xiatong <vanessazxt@gmail.com>
2020-05-22 16:01:01 +0200

    fix a bug

--- scripts/superuser_creator.py
+++ scripts/superuser_creator.py
@@ -3,7 +3,7 @@ from airflow import models, settings
 session = settings.Session()
 users = session.query(models.User).all()  # [admin, regular_user]
 
-users[1].superuser  # False
+# users[1].superuser  # False
 
 admin = users[0]
 admin.superuser = True
